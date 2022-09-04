1f9b9db095c7e6fbec763bd3a28d5bc238590fc9
vladutmargineanu <vladut.margineanu@gmail.com>
2022-09-04 17:23:58 +0300

    Inheritance & Abstract & Interfaces

---
+++ typescript-training/05-inheritance/Shape.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Shape = void 0;
+class Shape {
+    constructor(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+    }
+    get x() {
+        return this._x;
+    }
+    set x(value) {
+        this._x = value;
+    }
+    get y() {
+        return this._y;
+    }
+    set y(value) {
+        this._y = value;
+    }
+    // Method is public by default
+    getInfo() {
+        return `x=${this._x}, y=${this._y}`;
+    }
+}
+exports.Shape = Shape;
